c49cd0211c127c229d957d27c4cdd281da110c44
semperry <ryan.curtis88@gmail.com>
2019-12-16 12:16:37 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, render_template
+from twilio.rest import Client
+import config
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+  return render_template('home.html')
+
+
+@app.route('/send_sms', methods=['POST'])
+def send_sms():
+  phone_number = None
+  if request.method == 'POST':
+    phone_number = request.form['phone']
+    message = request.form['message']
+    account_sid = config.ACCOUNT_SID
+    auth_token = config.AUTH_TOKEN
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+                    .create(
+                      body= f"{message}",
+                      from_='+19153171361',
+                      to=f"+{phone_number}"
+                    )
+    print(message.sid)                    
+    return render_template('message_sent.html')
+  return home()
+
+
+if __name__ == "__main__":
+  app.run(debug=True)
